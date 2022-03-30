080c6310a008d4b4ef674db6ebf63d9b644a0919
Javier Osorio <javier.osorio@correounivalle.edu.co>
2022-03-30 10:49:34 -0400

    fix: test case

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,37 +1,32 @@
 import { hilbertTransform } from '../index';
 
 describe('test hilbert transform of cos(t)', () => {
+  const length = 50;
   it('test hilbert transform of cos -> sin function', () => {
-    const length = 50;
-    const cos = new Array(length).fill(0).map((_, i) => Math.cos(i));
-    const sin = new Array(length).fill(0).map((_, i) => Math.sin(i));
+    const cos = new Array(length)
+      .fill(0)
+      .map((_, i) => Math.cos((i * Math.PI) / 10));
+    const sin = new Array(length)
+      .fill(0)
+      .map((_, i) => Math.sin((i * Math.PI) / 10));
     const trs = hilbertTransform(cos);
-
-    expect(trs[5]).toBeCloseTo(sin[5], 1);
-    expect(trs[10]).toBeCloseTo(sin[10], 1);
-    expect(trs[11]).toBeCloseTo(sin[11], 1);
-    expect(trs[12]).toBeCloseTo(sin[12], 1);
-    expect(trs[13]).toBeCloseTo(sin[13], 1);
-    expect(trs[14]).toBeCloseTo(sin[14], 1);
-    expect(trs[15]).toBeCloseTo(sin[15], 1);
-    expect(trs[20]).toBeCloseTo(sin[20], 1);
-    expect(trs[30]).toBeCloseTo(sin[30], 1);
-    expect(trs[40]).toBeCloseTo(sin[40], 1);
+    // Excluding some points due to the edge effects
+    for (let i = 5; i < 45; i++) {
+      expect(trs[i]).toBeCloseTo(sin[i], 1);
+    }
   });
 
   it('test hilbert transform of sin -> -cos function', () => {
-    const length = 50;
-    const minusCos = new Array(length).fill(0).map((_, i) => -Math.cos(i));
-    const sin = new Array(length).fill(0).map((_, i) => Math.sin(i));
+    const minusCos = new Array(length)
+      .fill(0)
+      .map((_, i) => -Math.cos((i * Math.PI) / 10));
+    const sin = new Array(length)
+      .fill(0)
+      .map((_, i) => Math.sin((i * Math.PI) / 10));
     const trs = hilbertTransform(sin);
-    expect(trs[10]).toBeCloseTo(minusCos[10], 1);
-    expect(trs[11]).toBeCloseTo(minusCos[11], 1);
-    expect(trs[12]).toBeCloseTo(minusCos[12], 1);
-    expect(trs[13]).toBeCloseTo(minusCos[13], 1);
-    expect(trs[14]).toBeCloseTo(minusCos[14], 1);
-    expect(trs[15]).toBeCloseTo(minusCos[15], 1);
-    expect(trs[20]).toBeCloseTo(minusCos[20], 1);
-    expect(trs[30]).toBeCloseTo(minusCos[30], 1);
-    expect(trs[40]).toBeCloseTo(minusCos[40], 1);
+    // Excluding some points due to the edge effects
+    for (let i = 15; i < 35; i++) {
+      expect(trs[i]).toBeCloseTo(minusCos[i], 1);
+    }
   });
 });
